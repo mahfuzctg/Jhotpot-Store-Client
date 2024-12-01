c62d44b996f2a1da4c44d6c970d346e885f58797
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-02 00:35:56 +0600

    basic initialization done!

---
+++ src/app/(CommonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/src/components/shared/navbar/Navbar";
+
+export default function CommonLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
