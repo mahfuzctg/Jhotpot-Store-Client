c62dadb4e8eae790826e76206d08d5c7aa90cf18
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-02 19:43:38 +0600

    schema- added login and register schema

---
+++ src/schema/register.schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const registerValidationSchema = z.object({
+  name: z.string().min(1, "Please enter your name!"),
+  email: z.string().email("Please enter a valid email address!"),
+  password: z.string().min(1, "Please enter a password!"),
+});
+
+export default registerValidationSchema;
