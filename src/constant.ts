aa0a4ae31d9f78c04bc826d26c5e4ae1d2c02a60
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-10 06:31:00 +0600

    constant--added

---
+++ src/constant.ts
@@ -0,0 +1,13 @@
+export const protectedRoutes = [
+    "/login",
+    "/productDetails",
+    "/recentView",
+    "/shop",
+    "/checkout",
+    "/customer-dashboard",
+    "/vendor-dashboard",
+    "/admin-dashboard",
+    "/customer-dashboard/:page*",
+    "/vendor-dashboard/:page*",
+    "/admin-dashboard/:page*",
+  ];
