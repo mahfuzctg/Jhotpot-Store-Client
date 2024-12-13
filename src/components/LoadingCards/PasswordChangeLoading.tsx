3f1514a3facc3a8ba4015eac8206f0fa833ec481
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-14 03:55:43 +0600

    loading--pass-loading added

---
+++ src/components/LoadingCards/PasswordChangeLoading.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/self-closing-comp */
+const PasswordChangeLoading = () => {
+    return (
+      <div className="w-full p-6 rounded-lg shadow md:mt-5 sm:max-w-2xl sm:p-8 animate-pulse">
+        <div className="py-5">
+          <div className="h-6 bg-gray-800 rounded w-1/4 mb-2"></div>
+          <div className="h-10 bg-gray-700 rounded"></div>
+        </div>
+        <div className="pb-5">
+          <div className="h-6 bg-gray-800 rounded w-1/4 mb-2"></div>
+          <div className="h-10 bg-gray-700 rounded"></div>
+        </div>
+        <div className="pb-8">
+          <div className="h-6 bg-gray-800 rounded w-1/4 mb-2"></div>
+          <div className="h-10 bg-gray-700 rounded"></div>
+        </div>
+        <div className="flex justify-center items-center mb-10">
+          <div className="h-10 bg-gray-800 rounded w-full"></div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default PasswordChangeLoading;
