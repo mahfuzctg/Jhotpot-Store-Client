2442ed8ff3e8384162f98c1b9a7d85c4d4b3075b
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-14 20:17:28 +0600

    dashboard--vendor-layout added

--- src/app/(DashboardLayout)/(vendorDashboard)/vendor-dashboard/layout.tsx
+++ src/app/(DashboardLayout)/(vendorDashboard)/vendor-dashboard/layout.tsx
@@ -1,16 +1,81 @@
-"use client";
 
-import VendorProfileCard from "@/src/components/Cards/VendorProfileCard";
-import DashboardSectionTitle from "@/src/components/ui/components/DashboardSectionTitle";
+import type { Metadata } from "next";
+import {
+  Home,
+  BookOpenText,
+  ShoppingBag,
+  KeyRound,
+  House,
+  Package2,
+  TicketPercent,
+  Eye,
+} from "lucide-react";
+import Sidebar from "@/src/components/DashboardSidebar/Sidebar";
 
-const VendorHome = () => {
-  return (
-    <div>
-      <DashboardSectionTitle heading="Vendor Profile" />
-
-      <VendorProfileCard />
-    </div>
-  );
+export const metadata: Metadata = {
+  title: "Vendor Dashboard",
+  description:
+    "Discover, share, and explore travel stories, tips, and guides from a community of travel enthusiasts. Plan your next adventure with expert advice and unique insights into destinations around the world.",
 };
 
-export default VendorHome;
+export default function VendorDashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const adminLinks = [
+    {
+      label: "Dashboard Home",
+      href: "/vendor-dashboard",
+      icon: <Home size={20} />,
+    },
+    {
+      label: "My Products",
+      href: "/vendor-dashboard/myProducts",
+      icon: <ShoppingBag size={20} />,
+    },
+    {
+      label: "Order List",
+      href: "/vendor-dashboard/orderList",
+      icon: <BookOpenText size={20} />,
+    },
+    {
+      label: "Security",
+      href: "/customer-dashboard/security",
+      icon: <KeyRound size={20} />,
+    },
+  ];
+
+  const commonLinks = [
+    {
+      label: "Home",
+      href: "/",
+      icon: <House size={20} />,
+    },
+    {
+      label: "All Products",
+      href: "/allProducts",
+      icon: <Package2 size={20} />,
+    },
+    {
+      label: "Flash Sale",
+      href: "/flashSale",
+      icon: <TicketPercent size={20} />,
+    },
+    {
+      label: "Recent Viewed",
+      href: "/recentView",
+      icon: <Eye size={20} />,
+    },
+  ];
+
+  return (
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <Sidebar links={adminLinks} commonLinks={commonLinks} />
+
+      {/* Dashboard Content */}
+      <main className="flex-1 md:px-10 mt-6">{children}</main>
+    </div>
+  );
+}
